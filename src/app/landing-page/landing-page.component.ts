e34acf3353b66fd8061b13e47610a7ef4d2e4961
JoelImgu <44059689+joelimgu@users.noreply.github.com>
2020-11-08 14:59:48 +0100

    everithing working miecraft page missing

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+//import * as $ from "jquery";
 
 @Component({
   selector: 'app-landing-page',
@@ -13,8 +14,43 @@ export class LandingPageComponent implements OnInit {
 
   }
 
-  mail_click(){
-    alert("helo")
+
+  enable_mail_popup(){
+    document.getElementById('pop-up-backgrownd').style.display = 'inline';
+    document.getElementById('pop-up-backgrownd').style.animation = 'fade-in .5s forwards'
+
+    document.getElementById('mail-pop-up').style.animation = 'pop-in .5s forwards'
   }
+
+  enable_phone_popup(){
+    document.getElementById('pop-up-backgrownd').style.display = 'inline';
+    document.getElementById('pop-up-backgrownd').style.animation = 'fade-in .5s forwards'
+
+    document.getElementById('phone-pop-up').style.animation = 'pop-in .5s forwards'
+  }
+
+
+  copy_text_to_clopboard(text:string, htmlid: string, toolTipTextid: string) {
+    window.navigator.clipboard.writeText(text);
+    document.getElementById(toolTipTextid).innerHTML = "Copied! ;)";
+
+
+    let element = document.getElementById(htmlid);
+    element.classList.remove("copy-logo-animation");
+    void element.offsetWidth;
+    element.classList.add("copy-logo-animation");
+  }
+
+
+  copy_out(toolTipTextid: string){
+    document.getElementById(toolTipTextid).innerHTML = "Copy";
+  }
+
+  disable_mail_popup() {
+    document.getElementById('pop-up-backgrownd').style.animation = 'fade-out .5s forwards'
+    document.getElementById('mail-pop-up').style.animation = 'pop-out .5s forwards'
+    document.getElementById('phone-pop-up').style.animation = 'pop-out .5s forwards'
+  }
+
 
 }
